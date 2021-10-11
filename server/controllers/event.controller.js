a3ef071d2c9ad7f5eb7674eb7ad1e107436fd04b
renkoha28 <renkoha28@gmail.com>
2021-10-11 15:33:17 -0500

    added all crud backend events

--- server/controllers/event.controller.js
+++ server/controllers/event.controller.js
@@ -1,6 +1,5 @@
 const Event = require('../models/EventSchema');
 
-
 exports.create = function (req, res) {
     const event = new Event({
       name: req.body.data.name,
@@ -15,6 +14,93 @@ exports.create = function (req, res) {
           data: createdEvent,
           message: 'Success'
         });
+      })
+      .catch(function (err) {
+        return res.status(400).json({
+          status: 400,
+          message: err.message
+        });
+      });
+  }
+
+  exports.get = function (req, res) {
+    Event.findById(req.params.id)
+      .then(function (event) {
+        return res.status(200).json({
+          status: 200,
+          data: event,
+          message: "Success"
+        });
+      })
+      .catch(function (err) {
+        return res.status(400).json({
+          status: 400,
+          message: err.message
+        });
+      });
+  }
+  exports.list = function (req, res) {
+    Event.find()
+      .then(function (events) {
+        return res.status(200).json({
+          status: 200,
+          data: events,
+          message: 'Success'
+        })
+      })
+      .catch(function (err) {
+        return res.status(400).json({
+          status: 400,
+          message: err.message
+        });
+      });
+  }
+  exports.update = function (req, res) {
+    Event.findById(req.params.id)
+      .then(function (event) {
+        event.name = req.body.data.name || event.name;
+        event.date = req.body.data.date || event.date;
+        event.location = req.body.data.location || event.location;
+        event.weather = req.body.data.weather || event.weather;
+        event.save()
+          .then(function (updatedEvent) {
+            return res.status(200).json({
+              status: 200,
+              data: updatedEvent,
+              message: 'Success'
+            });
+          })
+          .catch(function (err) {
+            return res.status(400).json({
+              status: 400,
+              message: err.message
+            });
+          });
+      })
+      .catch(function (err) {
+        return res.status(400).json({
+          status: 400,
+          message: err.message
+        });
+      });
+  }
+  exports.delete = function (req, res) {
+    Event.findById(req.params.id)
+      .then(function (event) {
+        event.remove()
+          .then(function (deletedEvent) {
+            return res.status(200).json({
+              status: 200,
+              data: deletedEvent,
+              message: 'Success'
+            });
+          })
+          .catch(function (err) {
+            return res.status(400).json({
+              status: 400,
+              message: err.message
+            });
+          });
       })
       .catch(function (err) {
         return res.status(400).json({
--- server/routes/event.route.js
+++ server/routes/event.route.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const EventCtrl = require('../controllers/event.controller');
 const router = express.Router();
+router.get('/', EventCtrl.list);
+router.get('/:id', EventCtrl.get);
 router.post('/', EventCtrl.create);
+router.put('/:id', EventCtrl.update);
+router.delete('/:id', EventCtrl.delete);
 module.exports = router;
