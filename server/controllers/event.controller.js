6d9dadd919a7164321218298546c3f1d1c70f61f
renkoha28 <renkoha28@gmail.com>
2021-10-11 15:17:04 -0500

    adding the create event method

--- server/controllers/event.controller.js
+++ server/controllers/event.controller.js
@@ -1,1 +1,25 @@
 const Event = require('../models/EventSchema');
+
+
+exports.create = function (req, res) {
+    const event = new Event({
+      name: req.body.data.name,
+      date: req.body.data.date,
+      location: req.body.data.location,
+      weather: req.body.data.weather
+    });
+    event.save()
+      .then(function (createdEvent) {
+        return res.status(200).json({
+          status: 200,
+          data: createdEvent,
+          message: 'Success'
+        });
+      })
+      .catch(function (err) {
+        return res.status(400).json({
+          status: 400,
+          message: err.message
+        });
+      });
+  }
