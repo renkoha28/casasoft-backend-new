57f29d3c2047213a1a7b7a099d8e3d02555aacd9
renkoha28 <renkoha28@gmail.com>
2021-10-11 15:10:37 -0500

    adding event routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const eventRoutes = require('./event.route');
 const router = express.Router();
 router.get('/', function (req, res) {
   res.send('API works!');
 });
+router.use('/events', eventRoutes);
 module.exports = router;
